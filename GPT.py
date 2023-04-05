434e5ff38d7c28c91c1457f1fd668079af58543c
ilya <ilyasvinarenko98007@gmail.com>
2023-04-05 14:58:00 +0400

    functions for processing and responding to messages when openai is enabled

---
+++ GPT.py
@@ -0,0 +1,43 @@
+import openai, os, mongodb
+
+
+openai.api_key = os.environ.get('openai_api_key')
+
+current_contexts = {}
+
+
+async def get_response(message_text, chat_id, in_creating_process=0):
+    obj = mongodb.MongoForBotManager()
+    global current_contexts
+    if in_creating_process:
+        messages = [{'role': 'user', 'content': message_text}]
+        new_context = await obj.create_context_in_collection(str(chat_id), messages)
+        current_contexts[str(chat_id)] = new_context
+        answer = await get_answer(messages)
+        await obj.update_context(str(chat_id), new_context, {'role': 'assistant', 'content': answer})
+        return answer
+
+    current_context = current_contexts.get(str(chat_id))
+    if current_context is None:
+        return "Похоже включена функция openai\n" \
+               "Чтобы продолжить выберите контекст\n" \
+               "в меню /contexts"
+    await obj.update_context(str(chat_id), current_context, {'role': 'user', 'content': message_text})
+    messages = await obj.get_context(chat_id, current_contexts[str(chat_id)])
+    answer = await get_answer(messages)
+    await obj.update_context(str(chat_id), current_context, {'role': 'assistant', 'content': answer})
+    return answer
+
+
+async def get_answer(messages):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            temperature=0.4,
+        )
+        answer = response['choices'][0]['message']['content']
+        return answer
+    except Exception as error:
+        print('ERROR: ', error)
+        return f"Error: {error}"
